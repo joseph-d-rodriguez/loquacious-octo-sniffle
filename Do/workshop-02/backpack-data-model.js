adc559720701e13d3319d6f6e7a7b1753bbf13d4
Joseph Rodriguez <jrodriguez6@ebay.com>
2015-12-01 12:02:52 -0800

    Workshop #3... using promises... adding more/better data.

--- Do/workshop-02/backpack-data-model.js
+++ Do/workshop-02/backpack-data-model.js
@@ -1,20 +1,30 @@
 ;(function (angular) {
     angular.module('backpackData', [])
 
-    .controller('BackpackDataController', function($scope, backpackData) {
+    .controller('BackpackDataController', function($scope, backpackData, $timeout) {
         $scope.something = 'a variable for something';
 
-        backpackData.get().success(function(data) {
-            $scope.backpacks = data;
-        }).error(function(error) {
-           $scope.backpacksError = error;
-        });
+        $timeout(function() {
+            backpackData.get().then(function(response) {
+                $scope.backpacks = response.data;
+            }).catch(function(response) {
+               $scope.backpacksError = response.status + ': ' + response.data;
+            });
+        },
+        2000);
     })
 
     .service('backpackData', function getBackpackData($http) {
         var that = this;
         this.get = function(cb) {
             return $http.get('./backpacks.json');
+        };
+    })
+
+    .directive('displayBackpacks', function() {
+        return {
+            restrict: 'E',
+            templateUrl: './display-backpacks.html'
         };
     });
 })(window.angular);
